c6e17ce6e3dbdfc6327ebda5793e5acd462f0be5
Umar Bariev <ubariev@mail.ru>
2024-02-04 23:49:54 +0700

    Init commit

---
+++ src/main/java/com/umarbariev/sneakersshop/SneakersshopApplication.java
@@ -0,0 +1,13 @@
+package com.umarbariev.sneakersshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakersshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakersshopApplication.class, args);
+    }
+
+}
