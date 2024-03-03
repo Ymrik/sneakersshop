6d9dc780072cd92213b32723cb019b5b627c9cc3
Umar Bariev <ubariev@mail.ru>
2024-03-03 16:52:45 +0700

    просмотр заказов и изменения статуса заказов

--- src/main/java/com/umarbariev/sneakersshop/controller/AdminController.java
+++ src/main/java/com/umarbariev/sneakersshop/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.umarbariev.sneakersshop.controller;
 
 import com.umarbariev.sneakersshop.model.dto.ClientDto;
 import com.umarbariev.sneakersshop.model.dto.ClientOrdersStatusDto;
+import com.umarbariev.sneakersshop.model.dto.SearchUserOrderDto;
 import com.umarbariev.sneakersshop.model.dto.UpdateOrderStatusDto;
 import com.umarbariev.sneakersshop.model.dto.dictionary.DOrderStatusDto;
 import com.umarbariev.sneakersshop.security.UserDetailsServiceImpl;
@@ -13,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/admin")
@@ -31,6 +33,7 @@ public class AdminController {
     public String getClients(Model model) {
         List<ClientDto> clients = clientService.getAllClients();
         model.addAttribute("clients", clients);
+        model.addAttribute("searchUserOrders", new SearchUserOrderDto());
         return "admin-clients";
     }
 
@@ -38,8 +41,24 @@ public class AdminController {
     public String getOrders(Model model) {
         List<ClientOrdersStatusDto> orders = orderService.getAllOrders();
         List<DOrderStatusDto> statuses = orderService.getAllStatuses();
+        List<String> clientUsernames = clientService.getAllClients().stream().map(ClientDto::getUsername).toList();
+        model.addAttribute("clientsUsernames", clientUsernames);
+        model.addAttribute("searchUserOrders", new SearchUserOrderDto());
         model.addAttribute("statuses", statuses);
         model.addAttribute("orders", orders);
+        model.addAttribute("updateStatus", new UpdateOrderStatusDto());
+        return "admin-orders";
+    }
+
+    @PostMapping("/orders")
+    public String getOrdersByClient(@ModelAttribute("searchUserOrders") SearchUserOrderDto searchUserOrderDto, Model model) {
+        List<ClientOrdersStatusDto> orders = orderService.getClientOrders(searchUserOrderDto.getUsername());
+        List<DOrderStatusDto> statuses = orderService.getAllStatuses();
+        List<String> clientUsernames = clientService.getAllClients().stream().map(ClientDto::getUsername).toList();
+        model.addAttribute("statuses", statuses);
+        model.addAttribute("orders", orders);
+        model.addAttribute("searchUserOrders", new SearchUserOrderDto());
+        model.addAttribute("clientsUsernames", clientUsernames);
         model.addAttribute("updateStatus", new UpdateOrderStatusDto());
         return "admin-orders";
     }
---
+++ src/main/java/com/umarbariev/sneakersshop/model/dto/SearchUserOrderDto.java
@@ -0,0 +1,14 @@
+package com.umarbariev.sneakersshop.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchUserOrderDto {
+    private String username;
+}
